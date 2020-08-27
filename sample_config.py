b0e0ae4a27e0ddf9ff04772e326e52ccd00cd14e
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-08-26 21:27:13 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,22 +2,22 @@ import os
 
 class Config(object):
     # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
-    # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "e9357636-c381-483a-aa4e-b820c616277c")
+    to = input("Token :")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", to)
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 1152260))
+    API_HASH = os.environ.get("b61a446928fb880557d5de2dc2d87c3a")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1181258174").split())
     # Banned Unwanted Members..
-    BANNED_USERS = []
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 1572864000
+    TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
